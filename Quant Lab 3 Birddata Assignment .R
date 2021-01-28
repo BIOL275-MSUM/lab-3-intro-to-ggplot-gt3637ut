8d7c32940ef6ac7777530994aaadb88c1794239d
Hiba <hibachaudhry@Hibas-MacBook-Pro.local>
2021-01-28 14:17:16 -0600

    Adding files

---
+++ Quant Lab 3 Birddata Assignment .R
@@ -0,0 +1,13 @@
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+
+# bird_data ---------------------------------------------------------------
+
+
+library(auk)                          # load the auk package
+birds <- ebird_taxonomy %>%           # start with the ebird_taxonomy data
+  as_tibble() %>%                     # tibbles print better in the console
+  filter(category == "species")       # remove non-species taxa
+
---
+++ Quant Lab 3 Firefly_data Assignment .R
@@ -0,0 +1,29 @@
+
+# load packages  ----------------------------------------------------------
+
+library(tidyverse)
+
+firefly_data <- read_csv("https://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02q19FireflySpermatophoreMass.csv")
+firefly_data
+ggplot(data = firefly_data)
+
+ggplot(data = firefly_data) + geom_histogram(mapping = aes(x= spermatophoreMass))
+
+ggplot(data = firefly_data) +
+  geom_histogram(mapping = aes(x = spermatophoreMass), 
+                 fill = "#C5351B", color = "black") +
+  labs(x = "Spermatophore Mass", y = "Count") +
+  theme_classic() + 
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(color = "black", size = rel(1))
+  )
+  
+  
+
+    
+  
+  
+  
+  
+  
